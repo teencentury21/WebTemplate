c406b0ea9d60220b1fa859eb9b886981dd9dc3f4
Patrick <teencentury21@gmail.com>
2023-04-06 10:39:17 +0800

    add ignore file and enable

---
+++ SYS.Model/SQL/Default/Account.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SYS.Model.SQL.Default
+{
+    class Account
+    {
+    }
+}
